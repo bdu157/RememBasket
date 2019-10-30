ce9aaf85802b6bd62ae67de0d6d7281be3719222
Dongwoo Pae <dongwoopae@gmail.com>
2019-10-30 23:31:35 +0900

    added Password model and controller / storyborads are set up as well

---
+++ RememBasket/PasswordController.swift
@@ -0,0 +1,23 @@
+//
+//  PasswordController.swift
+//  RememBasket
+//
+//  Created by Dongwoo Pae on 10/30/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import Foundation
+
+class PasswordController {
+    
+    //CRUD
+    
+    //create password
+    
+    //read password
+    
+    //update password
+    
+    //delete password
+
+}
