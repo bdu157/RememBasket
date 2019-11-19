db1d8d30205ddf26429b9fae4786fd684f1e4b52
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-19 09:24:30 +0900

    added textfields and textview for password input

--- RememBasket/Model Controller/PasswordController.swift
+++ RememBasket/Model Controller/PasswordController.swift
@@ -10,13 +10,31 @@ import Foundation
 
 class PasswordController {
     
+    var passwords: [Password] = []
+    
     //CRUD
     //create password
+    func createPassword(title: String, userName: String, password: String, notes: String = "", createDate: Date = Date()) {
+        let password = Password(title: title, username: userName, password: password, notes: notes, createdDate: createDate)
+        self.passwords.append(password)
+    }
     
     //read password
     
     //update password
+    func updatePassword(for password: Password, changeTitleTo: String, changeUserNameTo: String, changePasswordTo: String, changeNotesTo: String?, modifiedDate: Date = Date()) {
+        guard let index = self.passwords.firstIndex(of: password) else {return}
+        var updatingPassword = self.passwords[index]
+        updatingPassword.title = changeTitleTo
+        updatingPassword.username = changeUserNameTo
+        updatingPassword.password = changePasswordTo
+        updatingPassword.notes = changeNotesTo
+        updatingPassword.createdDate = modifiedDate
+    }
     
     //delete password
-
+    func deletePassword(for password: Password) {
+        guard let index = self.passwords.firstIndex(of: password) else {return}
+        self.passwords.remove(at: index)
+    }
 }
--- RememBasket/View Controllers/PasswordDetailViewController.swift
+++ RememBasket/View Controllers/PasswordDetailViewController.swift
@@ -11,23 +11,54 @@ import UIKit
 class PasswordDetailViewController: UIViewController {
 
     @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var notesTextView: UITextView!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    var password: Password? {
+        didSet {
+            self.updateViews()
+        }
+    }
+    
+    var passwordController: PasswordController!
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
     }
-    */
+
     @IBAction func dismissButtonTapped(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let title = self.titleTextField.text,
+            let userName = self.emailTextField.text,
+            let password = self.passwordTextField.text else {return}
+        
+        self.passwordController.createPassword(title: title, userName: userName, password: password)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    //private methods
+    private func updateViews() {
+        guard let password = self.password else {return}
+        self.titleTextField?.text = password.title
+        self.emailTextField?.text = password.username
+        self.passwordTextField?.text = password.password
+        self.notesTextView?.text = password.notes
+    }
 }
