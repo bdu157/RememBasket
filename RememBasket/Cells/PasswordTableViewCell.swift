e67efd2992361539bc98f8139dbaa5fde4eafe49
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-25 17:19:50 +0900

    added if conditional for basketButton so titleLabel and id/pw label can be shown and hidden

--- RememBasket/Cells/PasswordTableViewCell.swift
+++ RememBasket/Cells/PasswordTableViewCell.swift
@@ -14,10 +14,12 @@ class PasswordTableViewCell: UITableViewCell {
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dividerLabel: UILabel!
-    @IBOutlet weak var basketButton: UIButton!
-
+    
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var basketButton: UIButton!
+    
     
     var password: Password? {
         didSet {
@@ -31,10 +33,8 @@ class PasswordTableViewCell: UITableViewCell {
         self.cellView.setShadowandCornerRadius()
         self.userNameLabel.alpha = 0.0
         self.passwordLabel.alpha = 0.0
-    }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        
+        self.basketButton.backgroundColor = .clear
     }
     
     
@@ -51,39 +51,83 @@ class PasswordTableViewCell: UITableViewCell {
     
     @IBAction func basketButtonTapped(_ sender: Any) {
         self.dividerAnimation()
+        basketButtonAnimation()
+    }
+    
+    //basketButton Animation
+    private func basketButtonAnimation() {
+        
+        //this will be used through UIImage -> open basket image and closed basket images
+        if self.basketButton.backgroundColor == .clear {
+            self.basketButton.backgroundColor = .orange
+        } else {
+            self.basketButton.backgroundColor = .clear
+        }
     }
     
     
     //animationforLabel
     private func dividerAnimation() {
         
-        UILabel.animateKeyframes(withDuration: 1.0, delay: 0, options: [], animations: {
+        if self.basketButton.backgroundColor == .clear {
             
-            UILabel.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
-                self.dividerLabel.center = CGPoint(x: self.logoImageView.center.x + (29+10), y: self.dividerLabel.center.y)
-            })
+            UILabel.animateKeyframes(withDuration: 1.0, delay: 0, options: [], animations: {
+                
+                UILabel.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
+                    self.dividerLabel.center = CGPoint(x: self.logoImageView.center.x + (29+10), y: self.dividerLabel.center.y)
+                })
+                
+                //titleLabel animation part
+                UILabel.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.2) {
+                    self.titleLabel.alpha = 0.0
+                }
+                
+                
+                //            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.1, animations: {
+                //                self.dividerLabel.center = self.dividerLabel.center
+                //            })
+                
+                UILabel.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
+                    self.dividerLabel.center = CGPoint(x: self.basketButton.center.x - (15+15), y: self.dividerLabel.center.y)
+                })
+                
+                UILabel.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.2) {
+                    self.userNameLabel.alpha = 1.0
+                    self.passwordLabel.alpha = 1.0
+                    self.userNameLabel.textColor = .orange
+                    self.passwordLabel.textColor = .orange
+                }
+                
+            }, completion: nil)
             
-            //titleLabel animation part
-            UILabel.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.2) {
-                self.titleLabel.alpha = 0.0
-            }
+        } else {
             
-            
-            //            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.1, animations: {
-            //                self.dividerLabel.center = self.dividerLabel.center
-            //            })
-            
-            UILabel.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
-                self.dividerLabel.center = CGPoint(x: self.basketButton.center.x - (15+15), y: self.dividerLabel.center.y)
-            })
-            
-            UILabel.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.2) {
-                self.userNameLabel.alpha = 1.0
-                self.passwordLabel.alpha = 1.0
-            }
-            
-        }, completion: nil)
-        
-        
+            UILabel.animateKeyframes(withDuration: 1.0, delay: 0, options: [], animations: {
+                
+                UILabel.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
+                    self.dividerLabel.center = CGPoint(x: self.logoImageView.center.x + (29+10), y: self.dividerLabel.center.y)
+                })
+                
+                //titleLabel animation part
+                UILabel.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.2) {
+                    self.userNameLabel.alpha = 0.0
+                    self.passwordLabel.alpha = 0.0
+                }
+                
+                
+                //            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.1, animations: {
+                //                self.dividerLabel.center = self.dividerLabel.center
+                //            })
+                
+                UILabel.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
+                    self.dividerLabel.center = CGPoint(x: self.basketButton.center.x - (15+15), y: self.dividerLabel.center.y)
+                })
+                
+                UILabel.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.2) {
+                    self.titleLabel.alpha = 1.0
+                }
+                
+            }, completion: nil)
+        }
     }
 }
