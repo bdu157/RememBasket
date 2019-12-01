09caaba117228f879bd1d1a47fa79b3e8b17e792
Dongwoo Pae <dongwoopae@gmail.com>
2019-12-01 15:35:18 +0900

    updated PasswordController and added CoreDataStack

--- RememBasket/Model Controller/PasswordController.swift
+++ RememBasket/Model Controller/PasswordController.swift
@@ -7,34 +7,58 @@
 //
 
 import Foundation
+import CoreData
 
 class PasswordController {
-    
-    var passwords: [Password] = []
     
     //CRUD
     //create password
     func createPassword(title: String, userName: String, password: String, notes: String?) {
-        //let password =
-        //self.passwords.append(password)
+        let _ = Password(title: title, username: userName, password: password, notes: notes)
+        saveToPersistentStore()
     }
     
     //read password
     
     //update password
     func updatePassword(for password: Password, changeTitleTo: String, changeUserNameTo: String, changePasswordTo: String, changeNotesTo: String?, modifiedDate: Date = Date()) {
-        guard let index = self.passwords.firstIndex(of: password) else {return}
-        self.passwords[index].title = changeTitleTo
-        self.passwords[index].username = changeUserNameTo
-        self.passwords[index].password = changePasswordTo
-        self.passwords[index].notes = changeNotesTo
-        //self.passwords[index].createdDate = modifiedDate
+        password.title = changeTitleTo
+        password.username = changeUserNameTo
+        password.password = changePasswordTo
+        password.notes = changeNotesTo
+        password.timestamp = modifiedDate
+        
+        saveToPersistentStore()
     }
     
     //delete password
     func deletePassword(for password: Password) {
-        guard let index = self.passwords.firstIndex(of: password) else {return}
-        self.passwords.remove(at: index)
+        let moc = CoreDataStack.shared.mainContext
+        moc.delete(password)
+        saveToPersistentStore()
     }
 
+    
+    //MARK: SaveToCoreDataStore - maincontext
+    func saveToPersistentStore() {
+        do {
+            let moc = CoreDataStack.shared.mainContext
+            try moc.save()
+        } catch {
+            NSLog("Error saving managed object context:\(error)")
+        }
+    }
+    
+    //MARK: SaveToCoreDataStore - backgroundContext
+    func saveToPersistentStoreBackgroundContext(bgcontext: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        bgcontext.performAndWait {
+            do {
+                try bgcontext.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error {throw error}
+    }
 }
