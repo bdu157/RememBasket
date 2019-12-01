5a74d743a91c2c9d6bdd274e2e36181cc680b45e
Dongwoo Pae <dongwoopae@gmail.com>
2019-12-01 16:00:28 +0900

    added NSFetchedResultsControllerDelegate methods for tableView

--- RememBasket/View Controllers/PasswordTableViewController.swift
+++ RememBasket/View Controllers/PasswordTableViewController.swift
@@ -69,13 +69,13 @@ class PasswordTableViewController: UITableViewController, NSFetchedResultsContro
     }
     
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let password = self.fetchedResultsController.object(at: indexPath)
-            self.passwordController.deletePassword(for: password)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-        }
-    }
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            let password = self.fetchedResultsController.object(at: indexPath)
+//            self.passwordController.deletePassword(for: password)
+//            tableView.deleteRows(at: [indexPath], with: .automatic)
+//        }
+//    }
     
     // MARK: - Navigation
     
@@ -97,4 +97,54 @@ class PasswordTableViewController: UITableViewController, NSFetchedResultsContro
         NotificationCenter.default.post(name: .needtoResetData, object: self)
     }
     
+    
+    //MARK: - NSfetchresultcontrollerDelegate - boilerPlate codes for coreData start here.
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.beginUpdates()
+    }
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        self.tableView.endUpdates()
+    }
+    
+    
+    //Sections
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    //Rows
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else {return}
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else {return}
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let oldIndexPath = indexPath,
+                let newIndexPath = newIndexPath else {return}
+            tableView.deleteRows(at: [oldIndexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else {return}
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        default:
+            break
+        }
+    }
 }
