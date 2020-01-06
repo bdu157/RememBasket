a2af70623ec01ea99b9adc49f838b5a5842868ba
Dongwoo Pae <dongwoopae@gmail.com>
2020-01-06 20:06:36 +0900

    added AppearanceHelper

--- Resources/AppDelegate.swift
+++ Resources/AppDelegate.swift
@@ -15,11 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UISearchBar.appearance().tintColor = .orange
-        UINavigationBar.appearance().tintColor = .orange
-        UITabBar.appearance().barTintColor = .white
-        UITabBar.appearance().tintColor = .orange
-    
+        AppearanceHelper.setDefaultAppearance()
         return true
     }
 
