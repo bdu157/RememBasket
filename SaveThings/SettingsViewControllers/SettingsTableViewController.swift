c2e43fa6536844a39ec8b16c9c76e855a9b9597a
Dongwoo Pae <dongwoopae@gmail.com>
2020-01-08 21:06:16 +0900

    added MessageUI API and implemented send email to developer button with MFMailComposeViewControllerDelegate

--- SaveThings/SettingsViewControllers/SettingsTableViewController.swift
+++ SaveThings/SettingsViewControllers/SettingsTableViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import StoreKit
+import MessageUI
 
 class SettingsTableViewController: UITableViewController {
     
     @IBOutlet weak var rateUsButton: UIButton!
+    @IBOutlet weak var emailSupportButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -77,8 +79,69 @@ class SettingsTableViewController: UITableViewController {
 
     @IBAction func rateUsButtonTapped(_ sender: Any) {
         SKStoreReviewController.requestReview()
+        self.rateUsButton.shake()
     }
-    @IBAction func contactButtonTapped(_ sender: Any) {
+
+    @IBAction func emailSupportButtonTapped(_ sender: Any) {
+        //this needs to be ran on a device
+        self.emailSupportButton.shake()
+        self.showMailComposer()
+    }
+    
+    private func showMailComposer() {
         
+        guard MFMailComposeViewController.canSendMail() else {
+            self.showNoEmailAlert()
+            return
+        }
+        
+        let composer = MFMailComposeViewController()
+        composer.mailComposeDelegate = self
+        composer.setToRecipients(["dongwoopae@gmail.com"])
+        composer.setSubject("Support Request")
+        
+        present(composer, animated: true)
+    }
+}
+
+extension SettingsTableViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        if let _ = error {
+            controller.dismiss(animated: true, completion: nil)
+            return
+        }
+        
+        switch result {
+        case .cancelled:
+            print("cancelled")
+        case .failed:
+            print("failed to send")
+        case .saved:
+            print("saved")
+        case .sent:
+            print("email sent")
+            self.showEmailSentAlert()
+        default:
+            print("default")
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    private func showEmailSentAlert() {
+        let alert = UIAlertController(title: "Email Sent", message: "We will get back to you shortly", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(okayAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true)
+    }
+    
+    private func showNoEmailAlert() {
+        let alert = UIAlertController(title: "No Email Available", message: "Please contact us at dongwoopae@gmail.com", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(okayAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true)
     }
 }
