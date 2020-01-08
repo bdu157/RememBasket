c1b49a56504cb146ff50cdb7100243ce92479d0a
Dongwoo Pae <dongwoopae@gmail.com>
2020-01-08 22:20:18 +0900

    added asyncafter for email sent alert so it can get called after dismiss the email view

--- SaveThings/SettingsViewControllers/SettingsTableViewController.swift
+++ SaveThings/SettingsViewControllers/SettingsTableViewController.swift
@@ -25,64 +25,6 @@ class SettingsTableViewController: UITableViewController {
         self.updateViews()
     }
 
-    // MARK: - Table view data source
-
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     @IBAction func rateUsButtonTapped(_ sender: Any) {
         SKStoreReviewController.requestReview()
         self.rateUsButton.shake()
@@ -141,7 +83,9 @@ extension SettingsTableViewController: MFMailComposeViewControllerDelegate {
             print("saved")
         case .sent:
             print("email sent")
-            self.showEmailSentAlert()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.showEmailSentAlert()
+            }
         default:
             print("default")
         }
@@ -151,9 +95,7 @@ extension SettingsTableViewController: MFMailComposeViewControllerDelegate {
     private func showEmailSentAlert() {
         let alert = UIAlertController(title: "Email Sent", message: "We will get back to you shortly", preferredStyle: .alert)
         let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(okayAction)
-        alert.addAction(cancelAction)
         self.present(alert, animated: true)
     }
     
